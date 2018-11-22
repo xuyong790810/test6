33161f25304e43afe2ad53a1c8dea0fdbcb11254
徐勇 <xuyong@xuyongdemacbook-pro.local>
2018-11-22 17:23:29 +0800

    修改

--- test7/test7/ViewController.swift
+++ test7/test7/ViewController.swift
@@ -13,6 +13,7 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         print("abc")
+        print("aaaaaaa")
         // Do any additional setup after loading the view, typically from a nib.
     }
 
